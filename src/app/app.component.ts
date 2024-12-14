0a84c0be91660a72e21aebca627d6b842d3621eb
wilderson <wilderson@uitecnologia.com.br>
2024-12-13 22:55:57 -0300

    packages e configs e app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { FirebaseService } from './services/firebase.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'sistema-ponto';
+  showNavbar = true;
+  user = this.firebaseService.getUser();
+
+  constructor(
+    private firebaseService: FirebaseService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Oculta o Navbar para rotas específicas
+        this.showNavbar = !['/login', '/cadastro'].includes(
+          event.urlAfterRedirects
+        );
+      }
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,24 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { environment } from './environment';
+import { provideHttpClient } from '@angular/common/http';
+
+// const firebaseConfig = environment.firebaseConfig;
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFunctions(() => getFunctions()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideHttpClient(),
+  ],
 };
+
+// if (environment.production) { enableProdMode(); }
